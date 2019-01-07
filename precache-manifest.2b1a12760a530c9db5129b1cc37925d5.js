57abd6cf61f9cf7fd64f2d3e6d5e7b579cae68b6
JohanGJensen <johancentralen@gmail.com>
2019-01-07 21:33:11 +0000

    Updates

---
+++ precache-manifest.2b1a12760a530c9db5129b1cc37925d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f232f42411914816bf71",
+    "url": "/ContactManager2/static/css/main.e73d0973.chunk.css"
+  },
+  {
+    "revision": "f232f42411914816bf71",
+    "url": "/ContactManager2/static/js/main.f232f424.chunk.js"
+  },
+  {
+    "revision": "ae96c4316938d37106ce",
+    "url": "/ContactManager2/static/css/1.d7af7afa.chunk.css"
+  },
+  {
+    "revision": "ae96c4316938d37106ce",
+    "url": "/ContactManager2/static/js/1.ae96c431.chunk.js"
+  },
+  {
+    "revision": "89887f091af6f3b0dba8",
+    "url": "/ContactManager2/static/js/runtime~main.89887f09.js"
+  },
+  {
+    "revision": "57a934953eb5308c579f1b80520e2b9c",
+    "url": "/ContactManager2/index.html"
+  }
+];
